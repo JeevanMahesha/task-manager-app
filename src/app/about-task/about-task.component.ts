1823b1925fe9b150f070712a7e8748df1bcbc5f8
Jeevan Mahesha <csejeevanm@gmail.com>
2024-06-29 12:45:09 +0530

    base required component creation

---
+++ src/app/about-task/about-task.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './about-task.component.html',
+  styleUrl: './about-task.component.scss'
+})
+export class AboutTaskComponent {
+
+}
