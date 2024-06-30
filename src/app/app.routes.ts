2a540a9aea4d9dc5b1c288d54afa78876186b424
Jeevan Mahesha <csejeevanm@gmail.com>
2024-06-30 22:36:18 +0530

    routerPath and pageNotFound added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,8 +6,16 @@ export const routes: Routes = [
     loadComponent: () => import('./task-list/task-list.component'),
   },
   {
+    path: 'about',
+    loadComponent: () => import('./about-task/about-task.component'),
+  },
+  {
     path: '',
     redirectTo: 'task',
     pathMatch: 'full',
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./page-not-found/page-not-found.component'),
   },
 ];
