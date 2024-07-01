8ba5fbb9942ddb1b0d8f2239b6c99dbc307eb49c
Jeevan Mahesha <csejeevanm@gmail.com>
2024-07-01 18:37:58 +0530

    ngx-toastr installed and config updated

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,15 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { TaskService } from './task.service';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), TaskService],
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    TaskService,
+    provideAnimations(),
+    provideToastr(),
+  ],
 };
